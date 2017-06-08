7b0318e23449b686f78143000b34f99f1a3c9faf
Yeri <arifyeripratama@gmail.com>
2017-06-08 09:49:53 +0700

    extract shapecontainer

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
 import React, { Component } from 'react';
 
-import Container from './components/Container';
-import Shape from './components/Shape';
-import Control from './components/Control';
+import ShapeContainer from './components/ShapeContainer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      shape: 'square'
-    };
-
-    this.changeSquare = this.changeSquare.bind(this);
-    this.changeCircle = this.changeCircle.bind(this);
-    this.changeTriangle = this.changeTriangle.bind(this);
-  }
-
-  changeCircle(e) {
-    this.setState((prevState, props) => ({
-      shape: 'circle'
-    }));
-  }
-
-  changeTriangle(e) {
-    this.setState((prevState, props) => ({
-      shape: 'triangle'
-    }));
-  }
-
-  changeSquare(e) {
-    this.setState((prevState, props) => ({
-      shape: 'square'
-    }));
-  }
-
   render() {
     return (
       <div>
-        <Container>
-          <Shape w={400} h={400} shape={this.state.shape} color="gold" />
-
-          <Control
-            changeCircle={this.changeCircle}
-            changeSquare={this.changeSquare}
-            changeTriangle={this.changeTriangle}
-          />
-        </Container>
+        <ShapeContainer />
+        <ShapeContainer />
       </div>
     );
   }
--- src/components/Shape.js
+++ src/components/Shape.js
@@ -5,8 +5,6 @@ export default ({ shape = 'square', color = 'gold', w = 200, h = 200 }) => {
 
   const shapeStyles = createStyles({ color, w, h });
 
-  console.log('shape', shape, shapeStyles);
-
   return (
     <div>
       <div style={shapeStyles[shape]}></div>
